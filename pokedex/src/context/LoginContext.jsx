c82e7353ca4cb55d655c7fb8c82f892c4b262b2e
Krzysiek <kmisiejuk15@gmail.com>
2024-12-12 22:33:51 +0100

    fix: login state

--- pokedex/src/context/LoginContext.jsx
+++ pokedex/src/context/LoginContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useState } from 'react';
 const LoginContext = createContext();
 
 const LoginProvider = ({ children }) => {
-	const [isLoggedIn, setIsLoggedIn] = useState(true);
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
 	const [loggedUserName, setLoggedUserName] = useState('User');
 	return (
 		<LoginContext.Provider
